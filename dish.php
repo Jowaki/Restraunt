5032f1f9db12827b3410c37abbbf90b6c445558c
Andrew Loaiza <a358l152@ku.edu>
2022-05-01 20:01:04 -0500

    ordering files, will implement db table loading next

---
+++ dish.php
@@ -0,0 +1,54 @@
+<?php
+
+// retreving data
+$db = $conn;
+$tableName = "DISH";
+$columns = ['DishID', 'Name','Price','Course'];
+$fetchData = fetch_data($db, $tableName, $columns);
+
+// fetch menu
+function fetch_data($db, $tableName, $columns)
+{
+  if(empty($db))
+  {
+    $msg= "Database connection error";
+  }
+
+  elseif (empty($columns) || !is_array($columns))
+  {
+    $msg="columns Name must be defined in an indexed array";
+  }
+
+  elseif(empty($tableName))
+  {
+    $msg= "Table Name is empty";
+  }
+
+  else
+  {
+    $columnName = implode(", ", $columns);
+    $query = "SELECT ".$columnName." FROM $tableName"." ORDER BY DishID ASC";
+    $result = $db->query($query);
+
+    if($result== true)
+    {
+      if ($result->num_rows > 0)
+      {
+        $row= mysqli_fetch_all($result, MYSQLI_ASSOC);
+        $msg= $row;
+      }
+      else
+      {
+        $msg= "No Data Found";
+      }
+    }
+
+    else
+    {
+      $msg= mysqli_error($db);
+    }
+  }
+  return $msg;
+}
+
+?>
