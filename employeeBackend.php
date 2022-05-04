1642b98588bfd07fc5af6efbb01fa46f501bf7b4
Andrew Loaiza <a358l152@ku.edu>
2022-05-03 19:11:11 -0500

    where Jowaki left off

--- employeeBackend.php
+++ employeeBackend.php
@@ -15,25 +15,90 @@ if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
     echo "<h1>Hello " .$row["Name"]. "\n</h1>";
   }
+  
 
   echo "<h2>Tables You Are Waiting: \n</h2>";
     $look = "SELECT TableID FROM WAITTABLE WHERE WaiterID='$waiter'";
     $result1 = $conn->query($look);
+    ?>
+    <form action="submitOrder.php" method="post"><?php
     if ($result1->num_rows > 0) {
         // output data of each row
-        while($row1 = $result1->fetch_assoc()) {
-            echo "<h3> * " .$row1["TableID"]. "\n</h3>";
+        while($row1 = $result1->fetch_assoc()) {?>
+            <?php echo $row1["TableID"]?> 
+            <?php
         }
+        ?>
+        <input type="text" id="tID" name="tID">
+        <?php
 
         // add order.php contents in here, make tables radio button
 
+        $query = "SELECT DishID, Name, Price, Course FROM DISH";
+
+        // execute query
+        $result = $conn->query($query);
 }
 ?>
 <html>
-    <form action="order.php" method="post">
+
+<!-- create form -->
+
+
+  <!-- make table -->
+  <table border="1" cellspacing="0" cellpadding="10">
+    <!-- header row -->
+    <tr>
+      <th>DishID</th>
+      <th>Name</th>
+      <th>Price</th>
+      <th>Course</th>
+      <th>Quantity</th>
+    </tr>
+
+  <?php
+    // load tuple as row
+    if ($result->num_rows > 0) {
+      while($data = $result->fetch_assoc())
+      {
+  ?>
+        <tr>
+          <td> <?php echo $data['DishID']; ?>  </td>
+          <td> <?php echo $data['Name'];   ?>  </td>
+          <td>$<?php echo $data['Price'];  ?>  </td>
+          <td> <?php echo $data['Course']; ?>  </td>
+          <td><input type="number" value=0 name=" <?php echo $data['DishID']; ?>"></td>
+        <tr>
+       
+  <?php
+      }
+
+       ?>
+       <?php
+
+    }
+
+    else
+    {
+  ?>
+        <tr>
+          <td colspan="8">No data found</td>
+        </tr>
+  <?php
+    }
+  ?>
+  </table>
+
+  <br>
+  <br>
+  <input type="submit" value="Generate Bill">
+
+
+
+  </form>
+    <!-- <form action="Order.php" method="post"> -->
     <body style="background-image: url('lettucepic.jpg');">
-    <button style="height:35px" type="submit" value="Submit ">Order</button>
-    </form>
+    
 </body>
 </html>
 
@@ -45,6 +110,11 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
-<html>
+<!-- <html>
 <body style="background-image: url('lettucepic.jpg');">
-</html>
+</html> -->
+
+
+
+
+<body style="background-image: url('lettucepic.jpg');"></body>
--- submitOrder.php
+++ submitOrder.php
@@ -5,8 +5,11 @@ ini_set("display_errors", 1);
 
 echo "testing";
 
+
+$test2 = $_POST["tID"];
 $test = $_POST["111"];
 
 echo " quantity for 111: " . $test;
+echo " quantity for Boo1: " . $test2;
 
 ?>
