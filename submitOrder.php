9328f5b331a6201be893b4946c252d967db565c8
Andrew Loaiza <a358l152@ku.edu>
2022-05-03 22:10:53 -0500

    finished receipt php

--- submitOrder.php
+++ submitOrder.php
@@ -16,12 +16,28 @@ echo "<title>Receipt</title>
       <header>
         <div class='company-name'>Lettuce Eat</div>
         <div class='receipt'>Receipt</div>
-      </header>";
+      </header>
 
-$billTotal = 0;
+      <div class='body'>
+        <p class='address'> 1603 W 15th <br> Jayhawk Tower-A Apt. 603 <br> Lawrence, KS 66044 </p>
+
+        <p>
+        Date: " . date('m/d/Y') . "<br>
+        Seating ID: " . $_POST["table"] . "<br>
+        </p>
+
+        <table class='receiptTable'>
+          <tr>
+            <th>Quantity</th>
+            <th>Food Item</th>
+            <th>Price</th>
+            <th>Total</th>
+          </tr>
+        ";
 
 $query = "SELECT DishID, Name, Price, Course FROM DISH";
 $result = $conn->query($query);
+$billTotal = 0;
 
 if ($result->num_rows > 0)
 {
@@ -29,36 +45,32 @@ if ($result->num_rows > 0)
   {
     $tempQty = (int)$_POST[$data['DishID']];
 
-    if ($tempQty >= 0)
+    if ($tempQty > 0)
     {
-      $billTotal += $tempQty * $data['Price'];
-    }
-    else
-    {
+      $itemTotal = $tempQty * $data['Price'];
+      $billTotal += $itemTotal;
+
+      echo "<tr>";
+      echo "<td>"  . $tempQty       . "</td>";
+      echo "<td>"  . $data['Name']  . "</td>";
+      echo "<td>$" . $data['Price'] . "</td>";
+      echo "<td>"  . $itemTotal     . "</td>";
+      echo "</tr>";
     }
   }
 }
 
-echo "<div class='body'>
+$query = "INSERT INTO `ORDERS` (`PayMethod`, `Bill`) VALUES('" . $_POST['payment'] . "', " . $billTotal . ")";
+$conn->query($query);
 
-        <p class='address'> 1603 W 15th <br> Jayhawk Tower-A Apt. 603 <br> Lawrence, KS 66044 </p>
-
-        Date: " . date('m/d/Y') . "<br>
-        Receipt #: ";
-
-
-
-echo    "<br><br>
-      </div>";
+echo "
+            <td colspan='2'></td>
+            <td><b>Order #" . mysqli_insert_id($conn) ." Total</b></td>
+            <td><b>" . $billTotal ."</b></td>
+          </table>
+        </div>";
 
 
-
-// $query = "INSERT INTO `ORDERS` (`PayMethod`, `Bill`) VALUES('" . $_POST['payment'] . "', " . $billTotal . ")";
-// $conn->query($query);
-
-// $myTable = $_POST["table"];
-
-// $test = $_POST["112"];
 
 
 ?>
